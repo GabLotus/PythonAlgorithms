427deb833faec965063a0f1d55ff1895b6346450
GabCampbellPoly <kenji901@gmail.com>
2017-07-19 23:05:48 -0400

    adding input functionality

--- Permutations/permutations.py
+++ Permutations/permutations.py
@@ -1,7 +1,4 @@
-s = "abbc"
-b = "abbcbbacbabcababcabbcabcabbcabcabbca"
-c = "abbc"
-d = "abbcbbac"
+import sys
 
 def verifyMultipliers(a, b, total_p):
 	if(a == b):
@@ -11,7 +8,31 @@ def verifyMultipliers(a, b, total_p):
 		
 		
 
-primes = [ 3, 5, 7, 11 ]
+#s = "abbc"
+#d = "abbcbbac"
+
+print(sys.argv[0])		
+if len(sys.argv) >= 3:
+	s = sys.argv[1]
+	d = sys.argv[2]
+	print( "looking for permutations of: " + s + " in: " + d)
+else:
+	s = "abbc"
+	d = "abbcbbac"
+	print("insuficient arguments. Running the program with inputs: " + s + " and: " + d)
+
+
+
+
+primes = [ 3,5,7,11,13,17,
+		   19,23,29,31,37,
+		   41,43,47,53,59,
+		   61,67,71,73,79,
+		   83,89,97,101,103,
+		   107,109,113,127,131,
+		   137,139,149,151,157,
+		   163,167,173,179,181,
+		   191,193,197,199 ]
 
 multiplied_s = 1
 multiplied_d = 1
@@ -21,15 +42,12 @@ for i in range(0, len(s)):
 	multiplied_s = multiplied_s * primes[ord(s[i]) - 97]
 	multiplied_d = multiplied_d * primes[ord(d[i]) - 97]
 	
-print(multiplied_s)
 
 total = verifyMultipliers(multiplied_d, multiplied_s, total)
 
 for x in range(0, len(d) - len(s)):
 
-	print(multiplied_d)
 	multiplied_d = (multiplied_d * primes[ord(d[x + len(s)]) - 97])/primes[ord(d[x]) - 97]
-	print(d[x + len(s)])
 	total = verifyMultipliers(multiplied_d, multiplied_s, total)
 
 	#for i in range (0,len(s)):
