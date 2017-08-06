1fa18074bca924d2c7bb7dcc9d5e08d5385d9a39
GabCampbellPoly <kenji901@gmail.com>
2017-08-06 01:26:46 -0400

    fixed the memoisation so it actually works

--- Misc/fibNMemo.py
+++ Misc/fibNMemo.py
@@ -1,25 +1,27 @@
 import sys
 
-def fib(n):
-	previousCalls = {}
+def fib(n, previousCalls):
+	
 	
 	if n == 1:
 		return 1
 	elif n == 0:
 		return 0
 	elif n in previousCalls:
+		
 		return previousCalls[n]
 	else:
-		previousCalls[n] = fib(n-1) + fib(n-2)
+		previousCalls[n] = fib(n-1, previousCalls) + fib(n-2, previousCalls)
 		return  previousCalls[n]
 
 		
 		
 n = 0
+previousCalls = {}
 		
 if len(sys.argv) < 2:
 	n = 8
 else:
 	n = int(sys.argv[1])
 	
-print(str(fib(n)))
+print(str(fib(n, previousCalls)))
