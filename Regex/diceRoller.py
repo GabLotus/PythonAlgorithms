aa6fc96aeed2dcd9f2a9943a307e00f1cb3769a4
GabCampbellPoly <kenji901@gmail.com>
2017-08-17 01:34:43 -0400

    added the possibility to do multiple rolls by adding more arguments

--- Regex/diceRoller.py
+++ Regex/diceRoller.py
@@ -22,20 +22,29 @@ def rollDices(mul, range_max):
 	total = 0
 	for i in range(mul):
 		roll = random.randint(1, range_max)
-		print(roll)
+		print("Roll: " + str(i + 1) + " = " + str(roll))
 		total += roll
 		
 	return total
 
 	
-s = "1d6"
+s = ["1d6"]
 if len(sys.argv) > 1:
-	s = sys.argv[1]
+	s = []
+	for roll in sys.argv:
+		if roll != sys.argv[0]:
+			s.append(roll)
+			
+			
 else:
 	print("No arguments, testing 1d6")
 
-multiplier, range_max = extractValues(s)
-	
-roll = rollDices(multiplier, range_max)
+total_rolls = 0
 
-print("The total obtained is: " + str(roll))
+for roll in s:	
+	multiplier, range_max = extractValues(roll)	
+	roll_result = rollDices(multiplier, range_max)
+	total_rolls += roll_result
+	print("Total for: " + str(roll) + " = " + str(roll_result))
+
+print("The total obtained for all rolls is: " + str(total_rolls))
