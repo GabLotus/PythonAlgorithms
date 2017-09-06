a70668138e302d6f806634be4e97714997b135ac
GabCampbellPoly <kenji901@gmail.com>
2017-09-06 15:08:33 -0400

    added fudge dice

--- Regex/diceRoller.py
+++ Regex/diceRoller.py
@@ -5,7 +5,7 @@ import random
 def extractValues(s):
 	regexp = re.compile(r"(?P<multiplier>[0-9]+)"
 						r"d"
-						r"(?P<range>[0-9]+)")
+						r"(?P<range>([0-9]+|[fF]))")
 
 	result = regexp.search(s)
 	if result == None:
@@ -15,15 +15,23 @@ def extractValues(s):
 		mul = result.group('multiplier')
 		range_max = result.group('range')
 	
-	return int(mul), int(range_max)
+	return int(mul), range_max
 	
 def rollDices(mul, range_max):
+	
 	print("Rolling for " + str(mul) + "d" + str(range_max))
 	total = 0
-	for i in range(mul):
-		roll = random.randint(1, range_max)
-		print("Roll: " + str(i + 1) + " = " + str(roll))
-		total += roll
+	if range_max != 'F' and range_max != 'f':
+		for i in range(mul):
+			roll = random.randint(1, range_max)
+			print("Roll: " + str(i + 1) + " = " + str(roll))
+			total += roll
+	else:
+		for i in range(mul):
+			roll = random.randint(0, 2) - 1
+			print("Roll: " + str(i + 1) + " = " + str(roll))
+			total += roll
+			
 		
 	return total
 
@@ -43,6 +51,9 @@ total_rolls = 0
 
 for roll in s:	
 	multiplier, range_max = extractValues(roll)	
+	if range_max != 'F' and range_max != 'f':
+		print(range_max)
+		range_max = int(range_max)
 	roll_result = rollDices(multiplier, range_max)
 	total_rolls += roll_result
 	print("Total for: " + str(roll) + " = " + str(roll_result))
