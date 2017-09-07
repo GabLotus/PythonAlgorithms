f9407611e41d173a6284dee73f5273434e703691
GabLotus <kenji901@gmail.com>
2017-09-06 20:43:39 -0400

    added some exercises

--- LinkedLists/listExercises.py
+++ LinkedLists/listExercises.py
@@ -11,6 +11,13 @@ class Node:
 			n = n.next
 		
 		n.next = end
+	
+	def appendConstructedNode(self, node):
+		n = self
+		while n.next is not None:
+			n = n.next
+		
+		n.next = node
 		
 
 def printList(head):
@@ -42,6 +49,23 @@ def generateReverseNumber(head):
 	mul = 10 ** (length - 1)
 	return generateNumber(head, mul, True)
 	
+def findIntersection(head1, head2):
+	listDict = {}
+	listDict[head1] = True
+	while head1.next is not None:
+		listDict[head1.next] = True
+		head1 = head1.next
+	
+	if head2  in listDict:
+		return head2
+	while head2.next is not None:
+		if head2.next  in listDict:
+			return head2.next
+		else:
+			head2 = head2.next
+	
+	return Node("nothing found")
+	
 	
 		
 		
@@ -53,8 +77,26 @@ list2 = Node(8)
 list2.appendNode(7)
 list2.appendNode(0)
 list2.appendNode(4)
-printList(list)
+
+
+
+interList = Node(1)
+interList.appendNode(2)
+interList.appendNode(3)
+interList2 = Node('a')
+interList2.appendNode('b')
+interList2.appendNode('c')
+interList2.appendNode('d')
+interList2.appendNode('e')
+interList.appendConstructedNode(interList2.next.next)
+
 
 print(generateNumber(list, 1, False))
 print(generateReverseNumber(list))
 print(getLength(list))
+
+
+printList(interList)
+intersectionFound = findIntersection(interList, interList2)
+print(intersectionFound.data)
+
