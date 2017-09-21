5beac27271d03827f2e46694435d444e79380c6b
GabLotus <kenji901@gmail.com>
2017-09-21 16:03:29 -0400

    reduced number of lines

--- BitwiseOperations/conversion.py
+++ BitwiseOperations/conversion.py
@@ -12,15 +12,12 @@ def numberOfFlips(number_a, number_b):
         lenght_a, lenght_b = lenght_b, lenght_a
     count = 0
     for x in range(0, lenght_a):
-        print(bin(number_a))
-        if (number_a & 1) ^ (number_b & 1) == 1:
+        if ((number_a >> x) & 1) ^ ((number_b >> x) & 1) == 1:
             count += 1
-        number_a = number_a >> 1
-        number_b = number_b >> 1
     
     return count
 
-print(numberOfFlips(0,1))
+print(numberOfFlips(5,2))
 
 
     
