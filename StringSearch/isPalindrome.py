678ff9fab61c91c5c734dd04be36b43dee13a032
GabLotus <kenji901@gmail.com>
2017-09-22 20:02:49 -0400

    modified isPalindrome into containsPalindrome because of ambiguous name. made a new isPalindrome function

--- StringSearch/isPalindrome.py
+++ StringSearch/isPalindrome.py
@@ -1,34 +1,16 @@
 import sys
 
 def isPalindrome(s):
-	d = {}
-	s_odd = len(s)%2
-	oddCounter = 0
-	evenCounter = 0
-	for c in s:
-		if c in d:
-			d[c] += 1
-		else:
-			d[c] = 1
-	
-	for c in s:
-		if c in d:
-			if d[c]%2 > 0:
-				oddCounter += 1
-			else:
-				evenCounter += 1
-			del d[c]
-		else:
-			continue
-	if oddCounter > s_odd:
-		print(oddCounter)
-		print(s_odd)
-		return False
-	else:
-		return True
+    for i in range(0,len(s)//2):
+        if s[i] != s[len(s) - 1 - i]:
+            return False
+    return True
 
 
-s = "aabcbaa"
+
+
+
+s = "aaabbcbbaaa"
 
 if len(sys.argv) > 1:
 	s = sys.argv[1]
@@ -36,4 +18,3 @@ else:
 	print("Insuficient arguments, testing algorithm with string: " + s)
 
 print(isPalindrome(s))
-
